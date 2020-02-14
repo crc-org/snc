6807cadc94c70fbf2efa2c0d570e30d6f69a639c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-02-14 09:45:26 +0530

    Added missing double quotes

--- snc.sh
+++ snc.sh
@@ -26,11 +26,11 @@ else
     if test -n "${OPENSHIFT_RELEASE_VERSION}"; then
         echo "Using release ${OPENSHIFT_RELEASE_VERSION} from local Git tags"
     else
-        OPENSHIFT_RELEASE_VERSION="$(curl -L "${MIRROR}/latest/release.txt | sed -n 's/^ *Version: //p')"
+        OPENSHIFT_RELEASE_VERSION="$(curl -L "${MIRROR}"/latest/release.txt | sed -n 's/^ *Version: //p')"
         if test -n "${OPENSHIFT_RELEASE_VERSION}"; then
             echo "Using release ${OPENSHIFT_RELEASE_VERSION} from the latest mirror"
         else
-            echo "Unable to determine an OpenShift release version.  You may want to set the OPENSHIFT_VERSION environment variable explicitly.
+            echo "Unable to determine an OpenShift release version.  You may want to set the OPENSHIFT_VERSION environment variable explicitly."
             exit 1
         fi
     fi
