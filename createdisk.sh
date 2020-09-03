07bb465a9d4b22d5303653cb14d35b9814f23e5f
Charro Gruver <charro_gruver@yahoo.com>
2020-09-03 07:55:15 -0400

    Update for OKD

--- createdisk.sh
+++ createdisk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OPENSHIFT_VERSION=4.5.0-0.okd-2020-08-12-020541
+
 export LC_ALL=C
 export LANG=C
 
@@ -58,8 +60,7 @@ function sparsify {
     guestfish --remote <<EOF
 add-drive $baseDir/$srcFile
 run
-luks-open $partition coreos-root
-mount /dev/mapper/coreos-root /
+mount $partition /
 zero-free-space /boot/
 EOF
     if [ $? -ne 0 ]; then
@@ -336,7 +337,7 @@ until ping -c1 api.${CRC_VM_NAME}.${BASE_DOMAIN} >/dev/null 2>&1; do
 done
 
 # Get the rhcos ostree Hash ID
-ostree_hash=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'cat /proc/cmdline | grep -oP "(?<=rhcos-).*(?=/vmlinuz)"')
+ostree_hash=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'cat /proc/cmdline | grep -oP "(?<=fedora-coreos-).*(?=/vmlinuz)"')
 
 # Get the rhcos kernel release
 kernel_release=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'uname -r')
@@ -345,7 +346,7 @@ kernel_release=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'uname -r')
 kernel_cmd_line=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'cat /proc/cmdline')
 
 # SCP the vmlinuz/initramfs from VM to Host in provided folder.
-${SCP} core@api.${CRC_VM_NAME}.${BASE_DOMAIN}:/boot/ostree/rhcos-${ostree_hash}/* $1
+${SCP} core@api.${CRC_VM_NAME}.${BASE_DOMAIN}:/boot/ostree/fedora-coreos-${ostree_hash}/* $1
 
 # Add a dummy network interface with internalIP
 ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- "sudo nmcli conn add type dummy ifname eth10 con-name internalEtcd ip4 ${INTERNAL_IP}/24  && sudo nmcli conn up internalEtcd"
