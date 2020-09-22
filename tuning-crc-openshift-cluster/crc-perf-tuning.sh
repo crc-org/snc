593e3058f2810d1323732d6c4bc2bc2fc4d39261
spaparaju <paparaju@gmail.com>
2020-09-22 16:43:52 +0530

    entry point for all the scripts to apply perf. tuning for CRC OpenShift components

---
+++ tuning-crc-openshift-cluster/crc-perf-tuning.sh
@@ -0,0 +1,51 @@
+OC=${OC:-oc}
+
+######
+##  Series of steps to inject necessary ENV variables and resources related changes for CRC ##
+#####
+
+
+######
+##  Enable v1alpha1/settings API for using Podpresets to set ENV variables while pods get created ##
+#####
+echo 'Enable Kube V1/alpha API .....'
+./enable-alpha-api.sh
+
+######
+##  Update manifest files for the Kube. control plane (static pods created by Kubelet). ##
+##  Thes changes inject ENV variables and changes to the resources related to CRC OpenShift components ##
+#####
+echo 'Update Kube control plane manifest files ......'
+./update-kube-controlplane.sh
+echo 'Wait for Kube API to be available after the restart (triggered from updating the manifest files) .....'
+sleep 180
+
+######
+##  Now that v1alpha1/setting API is enabled, create podpresets across all the namespaces ##
+#####
+echo 'Create podpresets ....'
+./trigger-podpresets.sh
+
+######
+##  Deploy Mutatingwebhook for specifying the appropriate resources to CRC OpenShift pods ##
+##  Source code for this Webhook is located at https://github.com/spaparaju/k8s-mutate-webhook
+#####
+echo 'Deploy MutatingWebhook for admission controller .....'
+oc apply -f admission-webhook.yaml
+echo 'Wait for  MutatingWebhook to be available ....'
+sleep 120
+
+######
+##  Now that Podpresets (across all the openshift- namespaces) Mutatingwebhook(cluster wide) are available, delete CRC OpenShift pods to get them recreated (by the respective operators) with the required ENV variables (from Podpresets) and required resources specified (from MutatingWebhook) ##
+#####
+echo 'Delete pods to inject ENV. and memroy/cpu initial requests ....'
+./delete-pods.sh
+echo 'Wait for pods to get recreated by the respective operators ....'
+sleep 840
+
+######
+##  Remove all the resources related MutatingWebhook (MutatingWebhook, service and the deployment for the webhook) ##
+#####
+echo 'Removing admission webhooks ..'
+${OC} delete MutatingWebhookConfiguration/mutateme svc/mutateme deploy/mutateme 
+echo 'All the perfomance settings been applied. DONE'
