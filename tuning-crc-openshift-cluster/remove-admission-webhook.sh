77c1a9596f6ef5d14c99860ca0f3371d55bcfa3b
spaparaju <paparaju@gmail.com>
2020-10-01 11:16:17 +0530

    updated comments

--- tuning-crc-openshift-cluster/remove-admission-webhook.sh
+++ tuning-crc-openshift-cluster/remove-admission-webhook.sh
@@ -1,3 +1,6 @@
-OC=${OC:-oc}
+## These admission webhook components (deployed with admission-webhook.yaml) enable updates to resource requests/limits to the Pod specs. 
+## The source-code for this admission controller is located at 'https://github.com/spaparaju/k8s-mutate-webhook'. 
+## Once all the OpenShift pods update their pod spec with the required resource requests/limits, these webhook related components are removed  so that the created CRC disk-image  does not contain any NON-standard OpenShift components.
 
+OC=${OC:-oc}
 ${OC} delete MutatingWebhookConfiguration/mutateme svc/mutateme deploy/mutateme
