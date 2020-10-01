9a574c2bd34b52c66eca73297a284f717d02ba56
spaparaju <paparaju@gmail.com>
2020-10-01 11:08:59 +0530

    Turn-on extended file attribute of 'immutability' on Kube control manifest files
    This step is required to avoid respective operators to overwrite the perf. related changes (at these manifest files) with the default manifest file content that gets shipped with the OCP payload

--- tuning-crc-openshift-cluster/make-kube-control-manifests-immutable.sh
+++ tuning-crc-openshift-cluster/make-kube-control-manifests-immutable.sh
@@ -1,6 +1,5 @@
 SSH_KEYS_OF_MASTER_NODE=../id_rsa_crc
 
-set -x
 SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEYS_OF_MASTER_NODE"
 
 ${SSH} core@master -- sudo chattr +i  /etc/kubernetes/manifests/kube-apiserver-pod.yaml 
