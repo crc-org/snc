30329e1f63158761faaead916daeaca1cbb1720e
spaparaju <paparaju@gmail.com>
2020-10-02 17:42:17 +0200

    Update manifest files of Kube control plane with required ENV variables and resource requests/limits values
    'update-kube-controlplane.sh' updates the Kube-control plane manifest
    files with required ENV variables (GOGC, GODEBUG=madvdontneed=1) and
    resource requests/limits. These Kube controlplane components include :
    Kube API server, Kubelet, Etcd, Kube-controller-manager and
    Kube-scheduler.

--- tuning-crc-openshift-cluster/crc-perf-tuning.sh
+++ tuning-crc-openshift-cluster/crc-perf-tuning.sh
@@ -5,4 +5,13 @@ OC=${OC:-oc}
 #####
 
 
+######
+##  Update manifest files for the Kube. control plane (static pods created by Kubelet). ##
+##  Thes changes inject ENV variables and changes to the resources related to CRC OpenShift components ##
+#####
+echo 'Update Kube control plane manifest files ......'
+tuning-crc-openshift-cluster/update-kube-controlplane.sh
+echo 'Wait for Kube API to be available after the restart (triggered from updating the manifest files) .....'
+sleep 180
+
 echo 'All the perfomance settings been applied. DONE'
