e9c45e804c1891fa90c701c4b37ded38c04fe3f4
spaparaju <paparaju@gmail.com>
2020-10-02 07:54:37 +0530

    Update manifest files of Kube control plane with required ENV variables and resource requests/limits values
    'update-kube-controlplane.sh' updates the Kube-control plane manifest files with required ENV variables (GOGC, GODEBUG=madvdontneed=1) and resource requests/limits. These Kube controlplane components include : Kube API server, Kubelet, Etcd, Kube-controller-manager and Kube-scheduler.
    Immutability is turned off prior to updating the files and immutability is turned on once the files are updated.Extended file attributes been used to set 'immutable' flag on Kube control plane manifests. This helps the respective not overwrite the manifest files with the default manifest file content (thats gets shipped with OCP core payload). These Kube control manifest files are located at '/etc/kubernetes/manifests' folder of the master VM of single node OpenShift cluster.

---
+++ tuning-crc-openshift-cluster/update-kube-controlplane.sh
@@ -0,0 +1,109 @@
+SSH_KEYS_OF_MASTER_NODE=../id_rsa_crc
+SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEYS_OF_MASTER_NODE"
+SCP="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEYS_OF_MASTER_NODE"
+
+JQ=${JQ:-jq}
+YQ=${YQ:-yq}
+
+ update_kube_apiserver_manifests() {
+    ${SSH} core@master -- sudo cat $1  > current_manifest.json
+    new_memory_value=$2
+    new_cpu_value=$3
+    ${JQ}  --arg new_memory_value "$new_memory_value" '(.spec.containers[].resources.requests.memory) |= $new_memory_value' current_manifest.json > updated_wth_memory_manifest.json
+    ${JQ}  --arg new_cpu_value "$new_cpu_value" '(.spec.containers[].resources.requests.cpu) |= $new_cpu_value' updated_wth_memory_manifest.json > updated_wth_cpu_manifest.json
+    ${JQ} '.spec.containers[].env |= . + [{"name": "GOGC", "value": "5"}, {"name": "GODEBUG", "value": "madvdontneed=1"}] ' updated_wth_cpu_manifest.json > updated_with_env_manifest.json
+    new_kubeapi_cpu_value=$4
+    ${JQ}  --arg new_kubeapi_cpu_value "$new_kubeapi_cpu_value" '(.spec.containers[] | select(.name == "kube-apiserver") | .resources.requests.cpu) |= $new_kubeapi_cpu_value' updated_with_env_manifest.json > updated_with_cpu_requests_manifest.json 
+    kubeapi_limit_cpu_value=$5
+    ${JQ}  --arg kubeapi_limit_cpu_value "$kubeapi_limit_cpu_value" '(.spec.containers[] | select(.name == "kube-apiserver") | .resources.limits.cpu) |= $kubeapi_limit_cpu_value' updated_with_cpu_requests_manifest.json  > final_manifest.json
+    cat final_manifest.json | ${JQ} -c > unformatted_final_manifest.json
+
+    ${SCP} -r unformatted_final_manifest.json core@master:/home/core/updated-kube-apiserver-manifest.yaml
+    ${SSH} core@master -- sudo cp /home/core/updated-kube-apiserver-manifest.yaml $1
+    ## Remove temp. files created
+    rm current_manifest.json updated_wth_memory_manifest.json updated_wth_cpu_manifest.json updated_with_env_manifest.json updated_with_cpu_requests_manifest.json final_manifest.json unformatted_final_manifest.json
+}
+
+ update_kube_controller_manifests() {
+    ${SSH} core@master -- sudo cat $1  > current_manifest.json
+    new_memory_value=$2
+    new_cpu_value=$3
+    ${JQ}  --arg new_memory_value "$new_memory_value" '(.spec.containers[].resources.requests.memory) |= $new_memory_value' current_manifest.json > updated_wth_memory_manifest.json
+    ${JQ}  --arg new_cpu_value "$new_cpu_value" '(.spec.containers[].resources.requests.cpu) |= $new_cpu_value' updated_wth_memory_manifest.json > updated_wth_cpu_manifest.json
+    ${JQ} '.spec.containers[].env |= . + [{"name": "GOGC", "value": "5"}, {"name": "GODEBUG", "value": "madvdontneed=1"}] ' updated_wth_cpu_manifest.json > updated_with_env_manifest.json
+    new_kube_controller_cpu_value=$4
+    ${JQ}  --arg new_kube_controller_cpu_value "$new_kube_controller_cpu_value" '(.spec.containers[] | select(.name == "kube-controller-manager") | .resources.requests.cpu) |= $new_kube_controller_cpu_value' updated_with_env_manifest.json  > updated_with_cpu_requests_manifest.json
+    kube_controller_limit_cpu_value=$5
+    ${JQ}  --arg kube_controller_limit_cpu_value "$kube_controller_limit_cpu_value" '(.spec.containers[] | select(.name == "kube-controller-manager") | .resources.limits.cpu) |= $kube_controller_limit_cpu_value' updated_with_cpu_requests_manifest.json  > final_manifest.json
+    cat final_manifest.json | ${JQ} -c > unformatted_final_manifest.json
+
+    ${SCP} -r unformatted_final_manifest.json core@master:/home/core/updated-kube-control-manager-manifest.yaml
+    ${SSH} core@master -- sudo cp /home/core/updated-kube-control-manager-manifest.yaml $1
+    ## Remove temp. files created
+    rm current_manifest.json updated_wth_memory_manifest.json updated_wth_cpu_manifest.json updated_with_env_manifest.json updated_with_cpu_requests_manifest.json final_manifest.json unformatted_final_manifest.json
+}
+
+ update_kube_scheduler_manifests() {
+    ${SSH} core@master -- sudo cat $1  > current_manifest.json
+    new_memory_value=$2
+    new_cpu_value=$3
+    ${JQ}  --arg new_memory_value "$new_memory_value" '(.spec.containers[].resources.requests.memory) |= $new_memory_value' current_manifest.json > updated_wth_memory_manifest.json
+    ${JQ}  --arg new_cpu_value "$new_cpu_value" '(.spec.containers[].resources.requests.cpu) |= $new_cpu_value' updated_wth_memory_manifest.json > updated_wth_cpu_manifest.json
+    ${JQ} '.spec.containers[].env |= . + [{"name": "GOGC", "value": "5"}, {"name": "GODEBUG", "value": "madvdontneed=1"}] ' updated_wth_cpu_manifest.json  > final_manifest.json
+    cat final_manifest.json | ${JQ} -c > unformatted_final_manifest.json
+
+    ${SCP} -r unformatted_final_manifest.json core@master:/home/core/updated-kube-scheduler-manifest.yaml
+    ${SSH} core@master -- sudo cp /home/core/updated-kube-scheduler-manifest.yaml $1
+    ## Remove temp. files created
+    rm current_manifest.json updated_wth_memory_manifest.json updated_wth_cpu_manifest.json  final_manifest.json unformatted_final_manifest.json
+}
+
+ update_etcd_manifests() {
+    ${SSH} core@master -- sudo cat $1  > current_manifest.yaml
+    new_memory_value=$2
+    new_cpu_value=$3
+    ${YQ} r -j current_manifest.yaml | ${JQ}  --arg new_memory_value "$new_memory_value" '(.spec.containers[].resources.requests.memory) |= $new_memory_value' -  | ${YQ} r -P - > updated_memory_manifest.yaml
+    ${YQ} r -j updated_memory_manifest.yaml | ${JQ}  --arg new_cpu_value "$new_cpu_value" '(.spec.containers[].resources.requests.cpu) |= $new_cpu_value' -  | ${YQ} r -P - > updated_cpu_manifest.yaml
+    ${YQ} r -j updated_cpu_manifest.yaml | ${JQ}  --arg new_memory_value "$new_memory_value" '(.spec.initContainers[].resources.requests.memory) |= $new_memory_value' -  | ${YQ} r -P - > updated_init_containers_memory_manifest.yaml
+    ${YQ} r -j updated_init_containers_memory_manifest.yaml | ${JQ}  --arg new_cpu_value "$new_cpu_value" '(.spec.initContainers[].resources.requests.cpu) |= $new_cpu_value' -  | ${YQ} r -P - > updated_init_containers_cpu_manifest.yaml
+    ${YQ} r -j updated_init_containers_cpu_manifest.yaml | ${JQ} '.spec.containers[].env |= . + [{"name": "GOGC", "value": "5"}, {"name": "GODEBUG", "value": "madvdontneed=1"}] ' -  | ${YQ} r -P - > updated_with_env_manifest.yaml
+    ${YQ} r -j updated_with_env_manifest.yaml | ${JQ} '.spec.initContainers[].env |= . + [{"name": "GOGC", "value": "5"}, {"name": "GODEBUG", "value": "madvdontneed=1"}] ' -  | ${YQ} r -P - > updated_init_containers_with_env_manifest.yaml
+    new_etcd_cpu_value=$4
+    ${YQ} r -j updated_init_containers_with_env_manifest.yaml | ${JQ}  --arg new_etcd_cpu_value "$new_etcd_cpu_value" '(.spec.containers[] | select(.name == "etcd") | .resources.requests.cpu) |= $new_etcd_cpu_value' -  > updated_with_new_cpu_request_manifest.yaml
+    etcd_cpu_limit_value=$4
+    ${YQ} r -j updated_with_new_cpu_request_manifest.yaml | ${JQ}  --arg etcd_cpu_limit_value "$etcd_cpu_limit_value" '(.spec.containers[] | select(.name == "etcd") | .resources.limits.cpu) |= $etcd_cpu_limit_value' -  > updated_final_manifest.yaml
+
+    ${SCP} -r updated_final_manifest.yaml core@master:/home/core/updated-etcd-final-manifest.yaml
+    ${SSH} core@master -- sudo cp /home/core/updated-etcd-final-manifest.yaml $1
+    ## Remove temp. files created
+    rm  current_manifest.yaml updated_memory_manifest.yaml updated_cpu_manifest.yaml updated_init_containers_memory_manifest.yaml updated_with_env_manifest.yaml updated_init_containers_with_env_manifest.yaml updated_init_containers_cpu_manifest.yaml updated_with_new_cpu_request_manifest.yaml updated_final_manifest.yaml
+}
+
+update_kubelet_systemd_service() {
+    ${SSH} core@master -- sudo cat $1  > current_kubelet.conf
+    ${YQ} w  current_kubelet.conf systemReserved.cpu $3  > updated_cpu_kubelet.conf
+    ${YQ} w  updated_cpu_kubelet.conf systemReserved.memory $2  > updated_memory_kubelet.conf
+    ${YQ} w  updated_memory_kubelet.conf containerLogMaxSize $4  > updated_final_kubelet.conf
+
+    ${SCP} -r updated_final_kubelet.conf core@master:/home/core/updated-kubelet.conf
+    ${SSH} core@master -- sudo cp /home/core/updated-kubelet.conf $1
+    ## clean up temp. files created
+    rm current_kubelet.conf updated_cpu_kubelet.conf updated_memory_kubelet.conf updated_final_kubelet.conf
+}
+
+
+echo '------------- Applying changes to Kubelet -----------'
+update_kubelet_systemd_service /etc/kubernetes/kubelet.conf 150Mi 200m 2Mi
+
+echo '------------- Applying changes to Kube API server  -----------'
+update_kube_apiserver_manifests   /etc/kubernetes/manifests/kube-apiserver-pod.yaml 10Mi 10m 500m 700m
+
+echo '------------- Applying changes to Kube Scheduler  -----------'
+update_kube_scheduler_manifests  /etc/kubernetes/manifests/kube-scheduler-pod.yaml 10Mi 15m
+
+echo '------------- Applying changes to Kube Control manager  -----------'
+update_kube_controller_manifests /etc/kubernetes/manifests/kube-controller-manager-pod.yaml 10Mi 10m 300m 400m
+
+echo '------------- Applying changes to Etcd -----------'
+update_etcd_manifests   /etc/kubernetes/manifests/etcd-pod.yaml 10Mi 10m 300m 500m
+
