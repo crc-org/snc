38c9f0b5eec044334e66e88528e97598bda2d55f
Christophe Fergeau <cfergeau@redhat.com>
2020-10-06 18:08:45 +0200

    standalone: Fix ssh key path
    The standalone scripts are expected to be run on a crc instance, the
    ssh key for these clusters is recreated during startup and located in
    ~/.crc/machines/crc/

--- tuning-crc-openshift-cluster/crc-env.sh
+++ tuning-crc-openshift-cluster/crc-env.sh
@@ -8,7 +8,7 @@ export YQ
 
 CRC_VM_NAME=${CRC_VM_NAME:-crc}
 BASE_DOMAIN=${CRC_BASE_DOMAIN:-testing}
-SSH_ARGS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $top_dir/id_rsa_crc"
+SSH_ARGS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.crc/machines/crc/id_rsa"
 SSH_HOST="core@api.${CRC_VM_NAME}.${BASE_DOMAIN}"
 SSH_CMD="ssh ${SSH_ARGS} ${SSH_HOST} --"
 SCP="scp ${SSH_ARGS}"
