ad604a93ebdb147b6fb6ff825afce54f440c5124
Christophe Fergeau <cfergeau@redhat.com>
2020-10-06 18:44:59 +0200

    Fix path to podpreset-template.yaml

--- tuning-crc-openshift-cluster/trigger-podpresets.sh
+++ tuning-crc-openshift-cluster/trigger-podpresets.sh
@@ -2,11 +2,12 @@
 
 set -exuo pipefail
 
+curdir="$(dirname $0)"
 for i in {1..60}
 do
 	namespace=`oc get ns |  awk 'NR=="'"$i"'"{print $1}'`
 	if [[ "$namespace" =~ "openshift-" ]]; then
-			sed 's/NAMESPACE_TO_REPLACE/"'"$namespace"'"/g' podpreset-template.yaml > trigger-podpreset.yaml
+			sed 's/NAMESPACE_TO_REPLACE/"'"$namespace"'"/g' "$curdir"/podpreset-template.yaml > trigger-podpreset.yaml
 			echo 'creating podpreset for the namespace: "'"$namespace"'"' 
 			${OC} apply -f trigger-podpreset.yaml
 	fi
