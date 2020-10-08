fa85903e974ce732648d092a66685ec2ca982975
Christophe Fergeau <cfergeau@redhat.com>
2020-10-08 13:17:08 +0200

    add retry loop when applying presets
    Sometimes the kubeapiserver restarts during the application of pod presets.
    Try a few times  before failing.

--- tuning-crc-openshift-cluster/trigger-podpresets.sh
+++ tuning-crc-openshift-cluster/trigger-podpresets.sh
@@ -6,8 +6,18 @@ curdir="$(dirname $0)"
 for namespace in $(oc get ns  -ojsonpath='{.items[*].metadata.name}')
 do
 	if [[ "$namespace" =~ "openshift-" ]]; then
+			local success
 			sed 's/NAMESPACE_TO_REPLACE/"'"$namespace"'"/g' "$curdir"/podpreset-template.yaml > trigger-podpreset.yaml
 			echo 'creating podpreset for the namespace: "'"$namespace"'"' 
-			${OC} apply -f trigger-podpreset.yaml
+			for i in {1..5}; do
+				if ${OC} apply -f trigger-podpreset.yaml ; then
+					success=1
+					break
+				fi
+				sleep 1
+			done
+			if [ $success != 1 ]; then
+				exit 1
+			fi
 	fi
 done
