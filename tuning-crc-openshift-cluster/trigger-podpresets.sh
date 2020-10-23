0d7ed65dd49e377fdff9c246665fd65eef97d251
spaparaju <paparaju@gmail.com>
2020-10-23 15:02:11 +0530

    removed local scope for the variable (success) to resolve script execution failures

--- tuning-crc-openshift-cluster/trigger-podpresets.sh
+++ tuning-crc-openshift-cluster/trigger-podpresets.sh
@@ -6,7 +6,7 @@ curdir="$(dirname $0)"
 for namespace in $(oc get ns  -ojsonpath='{.items[*].metadata.name}')
 do
 	if [[ "$namespace" =~ "openshift-" ]]; then
-			local success
+			success=0
 			sed 's/NAMESPACE_TO_REPLACE/"'"$namespace"'"/g' "$curdir"/podpreset-template.yaml > trigger-podpreset.yaml
 			echo 'creating podpreset for the namespace: "'"$namespace"'"' 
 			for i in {1..5}; do
