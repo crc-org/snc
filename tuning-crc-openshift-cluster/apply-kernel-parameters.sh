b21a17d860aee36f2bf8efc76af64334c0f513c7
spaparaju <paparaju@gmail.com>
2020-10-24 20:06:13 +0530

    change the SSH host to apply grubby cmd

--- tuning-crc-openshift-cluster/apply-kernel-parameters.sh
+++ tuning-crc-openshift-cluster/apply-kernel-parameters.sh
@@ -2,9 +2,6 @@
 
 set -exuo pipefail
 
-SSH_KEYS_OF_MASTER_NODE=../id_rsa_crc
-SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEYS_OF_MASTER_NODE"
-
-${SSH} core@master -- sudo grubby --update-kernel=ALL --args="transparent_hugepage=never "
-${SSH} core@master -- sudo grubby --update-kernel=ALL --args="vm.swappiness=30 "
+${SSH_GRUBBY_CMD} sudo grubby --update-kernel=ALL --args="transparent_hugepage=never "
+${SSH_GRUBBY_CMD} sudo grubby --update-kernel=ALL --args="vm.swappiness=30"
 
--- tuning-crc-openshift-cluster/crc-env.sh
+++ tuning-crc-openshift-cluster/crc-env.sh
@@ -11,6 +11,8 @@ BASE_DOMAIN=${CRC_BASE_DOMAIN:-testing}
 SSH_ARGS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.crc/machines/crc/id_rsa"
 SSH_HOST="core@api.${CRC_VM_NAME}.${BASE_DOMAIN}"
 SSH_CMD="ssh ${SSH_ARGS} ${SSH_HOST} --"
+MASTER_HOST="master"
+SSH_GRUBBY_CMD="ssh ${SSH_ARGS} ${MASTER_HOST} --"
 SCP="scp ${SSH_ARGS}"
 
 export SSH_CMD
