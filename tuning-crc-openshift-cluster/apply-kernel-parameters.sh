c78da937622ff9e8cb713d35aa0dbc3ccdcc50c8
spaparaju <paparaju@gmail.com>
2020-10-24 22:04:50 +0530

    to retrigger tests

--- tuning-crc-openshift-cluster/apply-kernel-parameters.sh
+++ tuning-crc-openshift-cluster/apply-kernel-parameters.sh
@@ -2,6 +2,6 @@
 
 set -exuo pipefail
 
-${SSH_GRUBBY_CMD} sudo grubby --update-kernel=ALL --args="transparent_hugepage=never "
+${SSH_GRUBBY_CMD} sudo grubby --update-kernel=ALL --args="transparent_hugepage=never"
 ${SSH_GRUBBY_CMD} sudo grubby --update-kernel=ALL --args="vm.swappiness=30"
 
