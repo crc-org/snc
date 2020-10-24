d7b86b2d99b11a74e9e8ae43e7aaca5bbdfe9d67
spaparaju <paparaju@gmail.com>
2020-10-24 17:56:56 +0530

    debug statements to trigger grubby on master node

--- tuning-crc-openshift-cluster/apply-kernel-parameters.sh
+++ tuning-crc-openshift-cluster/apply-kernel-parameters.sh
@@ -2,5 +2,9 @@
 
 set -exuo pipefail
 
-${SSH_CMD} sudo grubby --update-kernel=ALL --args="transparent_hugepage=never "
-${SSH_CMD} sudo grubby --update-kernel=ALL --args="vm.swappiness=30"
+SSH_KEYS_OF_MASTER_NODE=../id_rsa_crc
+SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEYS_OF_MASTER_NODE"
+
+${SSH} core@master -- sudo grubby --update-kernel=ALL --args="transparent_hugepage=never "
+${SSH} core@master -- sudo grubby --update-kernel=ALL --args="vm.swappiness=30 "
+
