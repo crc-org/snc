7049884acc5e1ddbc78339c93b381cdb4b24a70c
spaparaju <paparaju@gmail.com>
2020-10-26 16:12:49 +0530

    fix swap related error, turned off setting kernel parameters as rpm-ostree kargs needs some fix

--- tuning-crc-openshift-cluster/apply-kernel-parameters.sh
+++ tuning-crc-openshift-cluster/apply-kernel-parameters.sh
@@ -3,5 +3,4 @@
 set -exuo pipefail
 
 ${SSH_CMD} sudo rpm-ostree kargs --append=transparent_hugepage=never
-${SSH_CMD} sudo rpm-ostree kargs --append=vm.swappiness=30
 
--- tuning-crc-openshift-cluster/enable-swap-space.sh
+++ tuning-crc-openshift-cluster/enable-swap-space.sh
@@ -6,5 +6,5 @@ ${SSH_CMD} sudo fallocate -l 3G /var/home/core/crc-swapfile
 ${SSH_CMD} sudo chmod 600 /var/home/core/crc-swapfile
 ${SSH_CMD} sudo mkswap /var/home/core/crc-swapfile
 ${SSH_CMD} sudo swapon /var/home/core/crc-swapfile
-${SSH_CMD} sudo "echo '/var/home/core/crc-swapfile none swap sw 0 0' | sudo tee -a /etc/fstab"
+${SSH_CMD} sudo "echo '/var/home/core/crc-swapfile          swap            swap    defaults        0 0' | sudo tee -a /etc/fstab"
 ${SSH_CMD} sudo swapon -s
