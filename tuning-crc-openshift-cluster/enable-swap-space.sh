af764ba498960bfca374fc60622024cb2fceeb81
spaparaju <paparaju@gmail.com>
2020-10-28 00:21:46 +0530

    fix swap space creation as previous attempts did not factor in immutable FS

--- tuning-crc-openshift-cluster/enable-swap-space.sh
+++ tuning-crc-openshift-cluster/enable-swap-space.sh
@@ -2,9 +2,22 @@
 
 set -exuo pipefail
 
-${SSH_CMD} sudo fallocate -l 3G /var/home/core/crc-swapfile
-${SSH_CMD} sudo chmod 600 /var/home/core/crc-swapfile
-${SSH_CMD} sudo mkswap /var/home/core/crc-swapfile
-${SSH_CMD} sudo swapon /var/home/core/crc-swapfile
-${SSH_CMD} sudo "echo '/var/home/core/crc-swapfile          swap            swap    defaults        0 0' | sudo tee -a /etc/fstab"
+${SSH_CMD} sudo mkdir -p /var/home/core/vm
+${SSH_CMD} sudo fallocate -l 3G /var/home/core/vm/crc-swapfile
+${SSH_CMD} sudo chmod 600 /var/home/core/vm/crc-swapfile
+${SSH_CMD} sudo mkswap /var/home/core/vm/crc-swapfile
+
+sudo tee -a /etc/systemd/system/var-home-core-vm-swapfile.swap > /dev/null <<EOT
+[Unit]
+Description=Turn on swap
+
+[Swap]
+What=/var/home/core/vm/crc-swapfile
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+${SSH_CMD} sudo systemctl --now enable /etc/systemd/system/var-home-core-vm-swapfile.swap
+${SSH_CMD} sudo systemctl status var-home-core-vm-swapfile.swap
 ${SSH_CMD} sudo swapon -s
