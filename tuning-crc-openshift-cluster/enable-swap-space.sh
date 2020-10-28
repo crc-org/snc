20c0bf0a544491bc00ca76970ec2385e27aa0fba
spaparaju <paparaju@gmail.com>
2020-10-28 22:14:46 +0530

    include ssh_cmd while writing systemd unit file

--- tuning-crc-openshift-cluster/enable-swap-space.sh
+++ tuning-crc-openshift-cluster/enable-swap-space.sh
@@ -7,7 +7,7 @@ ${SSH_CMD} sudo fallocate -l 3G /var/home/core/vm/swapfile
 ${SSH_CMD} sudo chmod 600 /var/home/core/vm/swapfile
 ${SSH_CMD} sudo mkswap /var/home/core/vm/swapfile
 
-echo $'[Unit] \n Description=Turn on swap \n [Swap] \n What=/var/home/core/vm/swapfile \n [Install] \n WantedBy=multi-user.target'  | sudo tee -a /etc/systemd/system/var-home-core-vm-swapfile.swap
+echo $'[Unit] \n Description=Turn on swap \n [Swap] \n What=/var/home/core/vm/swapfile \n [Install] \n WantedBy=multi-user.target'  | ${SSH_CMD} sudo tee -a /etc/systemd/system/var-home-core-vm-swapfile.swap
 ${SSH_CMD} sudo systemctl --now enable /etc/systemd/system/var-home-core-vm-swapfile.swap
 ${SSH_CMD} sudo systemctl status var-home-core-vm-swapfile.swap
 ${SSH_CMD} sudo swapon -s
