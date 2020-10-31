b49d30ee916f1d90bb1d8a95d1f8c3e65a037a77
spaparaju <paparaju@gmail.com>
2020-10-31 16:41:50 +0530

    corrected space issue in /etc/fstab file

--- tuning-crc-openshift-cluster/enable-swap-space.sh
+++ tuning-crc-openshift-cluster/enable-swap-space.sh
@@ -8,7 +8,7 @@ ${SSH_CMD} sudo chmod 600 /var/home/core/vm/swapfile
 ${SSH_CMD} sudo mkswap /var/home/core/vm/swapfile
 
 echo $'[Unit] \n Description=Turn on swap \n [Swap] \n What=/var/home/core/vm/swapfile \n [Install] \n WantedBy=multi-user.target'  | ${SSH_CMD} sudo tee -a /etc/systemd/system/var-home-core-vm-swapfile.swap
-echo '/var/home/core/vm/swapfile              swap                    swap    defaults        0 0'  | ${SSH_CMD} sudo tee -a /etc/fstab
+echo '/var/home/core/vm/swapfile               swap                    swap    defaults        0 0'  | ${SSH_CMD} sudo tee -a /etc/fstab
 
 ${SSH_CMD} sudo systemctl --now enable /etc/systemd/system/var-home-core-vm-swapfile.swap
 ${SSH_CMD} sudo systemctl status var-home-core-vm-swapfile.swap
