0bf12e220b280983fbc2a17b75d94a041497f458
spaparaju <paparaju@gmail.com>
2020-10-31 22:11:17 +0530

    fixed premature exit from the shell script

--- tuning-crc-openshift-cluster/trigger-podpresets.sh
+++ tuning-crc-openshift-cluster/trigger-podpresets.sh
@@ -6,18 +6,8 @@ curdir="$(dirname $0)"
 for namespace in $(oc get ns  -ojsonpath='{.items[*].metadata.name}')
 do
 	if [[ "$namespace" =~ "openshift-" ]]; then
-			success=0
 			sed 's/NAMESPACE_TO_REPLACE/"'"$namespace"'"/g' "$curdir"/podpreset-template.yaml > $curdir/trigger-podpreset.yaml
 			echo 'creating podpreset for the namespace: "'"$namespace"'"' 
-			for i in {1..2}; do
-				if ${OC} apply -f $curdir/trigger-podpreset.yaml ; then
-					success=1
-					break
-				fi
-				sleep 1
-			done
-			if [ $success != 1 ]; then
-				exit 1
-			fi
+			${OC} apply -f $curdir/trigger-podpreset.yaml
 	fi
 done
