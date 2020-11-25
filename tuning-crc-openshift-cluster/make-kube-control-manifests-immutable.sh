35bcc7147955b13aa16da33a554fddd8bcdf0047
spaparaju <paparaju@gmail.com>
2020-11-25 16:43:23 +0530

    Kube control plane manifests are updated with the perf. changes (ENV and initial resource requests)

---
+++ tuning-crc-openshift-cluster/make-kube-control-manifests-immutable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -exuo pipefail
+${SSH_CMD} sudo chattr +i  /etc/kubernetes/manifests/kube-apiserver-pod.yaml
+${SSH_CMD} sudo chattr +i  /etc/kubernetes/manifests/kube-controller-manager-pod.yaml
+${SSH_CMD} sudo chattr +i  /etc/kubernetes/manifests/kube-scheduler-pod.yaml
+${SSH_CMD} sudo chattr +i  /etc/kubernetes/manifests/etcd-pod.yaml
