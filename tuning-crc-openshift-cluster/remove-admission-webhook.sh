2603d6e6cf172d2441a73b2918c67d49e332474c
spaparaju <paparaju@gmail.com>
2020-11-25 16:41:21 +0530

    Mutating admission webhooks are created under crc-perf-tuning.sh to update the intial resource requests. These admission webhooks are removed once all the perf. changes are applied

---
+++ tuning-crc-openshift-cluster/remove-admission-webhook.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+## These admission webhook components (deployed with admission-webhook.yaml) enable updates to resource requests/limits to the Pod specs. 
+## The source-code for this admission controller is located at 'https://github.com/spaparaju/k8s-mutate-webhook'. 
+## Once all the OpenShift pods update their pod spec with the required resource requests/limits, these webhook related components are removed  so that the created CRC disk-image  does not contain any NON-standard OpenShift components.
+
+${OC} delete MutatingWebhookConfiguration/crc-mutating svc/crc-mutating deploy/crc-mutating
