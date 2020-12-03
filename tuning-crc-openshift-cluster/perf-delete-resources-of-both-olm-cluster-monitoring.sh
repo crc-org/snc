76092b0cea457c7c30e4186cc59b69ae8c32fb95
spaparaju <paparaju@gmail.com>
2020-12-03 12:37:45 +0530

    rebase issues, rearranged cvo override yaml files that get applied at CRC runtime

--- tuning-crc-openshift-cluster/perf-delete-resources-of-both-olm-cluster-monitoring.sh
+++ tuning-crc-openshift-cluster/perf-delete-resources-of-both-olm-cluster-monitoring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
-oc  --kubeconfig  /opt/kubeconfig  patch clusterversion version --type json -p "$(cat   /var/home/core/perf-cvo-override-remove-both-olm-cluster-monitoring.yaml)"
+oc  --kubeconfig  /opt/kubeconfig  patch clusterversion version --type json -p "$(cat   /var/home/core/perf-cvo-override-remove-olm.yaml)"
 
 oc  --kubeconfig /opt/kubeconfig delete ns openshift-operator-lifecycle-manager &> /dev/null  &
 oc  --kubeconfig /opt/kubeconfig delete ns openshift-marketplace  &> /dev/null &
