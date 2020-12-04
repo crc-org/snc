25b418195b4391122dfad5d0a583775655ee53d2
spaparaju <paparaju@gmail.com>
2020-12-04 21:34:05 +0530

    fix namespaces getting stuck in Terminating condition & not all pods backedup by deployments are getting deleted

--- tuning-crc-openshift-cluster/perf-delete-resources-of-cluster-monitoring.sh
+++ tuning-crc-openshift-cluster/perf-delete-resources-of-cluster-monitoring.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
   
 oc  --kubeconfig   /opt/kubeconfig  patch clusterversion version --type json -p "$(cat   /var/home/core/perf-cvo-override-remove-cluster-monitoring.yaml)"
+oc  --kubeconfig /opt/kubeconfig delete pods --all --grace-period=0 --force  -n openshift-monitoring
 oc  --kubeconfig /opt/kubeconfig delete ns openshift-monitoring  &> /dev/null &
 exit 0
--- tuning-crc-openshift-cluster/perf-delete-resources-of-olm.sh
+++ tuning-crc-openshift-cluster/perf-delete-resources-of-olm.sh
@@ -2,6 +2,8 @@
   
 oc  --kubeconfig   /opt/kubeconfig  patch clusterversion version --type json -p "$(cat  /var/home/core/perf-cvo-override-remove-olm.yaml)"
 
+oc  --kubeconfig /opt/kubeconfig delete pods --all --grace-period=0 --force  -n openshift-operator-lifecycle-manager
+oc  --kubeconfig /opt/kubeconfig delete pods --all --grace-period=0 --force  -n openshift-marketplace
 oc  --kubeconfig /opt/kubeconfig delete ns openshift-operator-lifecycle-manager &> /dev/null  &
 oc  --kubeconfig /opt/kubeconfig delete ns openshift-marketplace &> /dev/null  &
 exit 0
