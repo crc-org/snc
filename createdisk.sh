869a2f44135860dfe12aa93acdac0ca23c6ad861
Mick Tarsel <mtarsel@gmail.com>
2021-02-02 14:02:01 -0800

    Create prepare_hyperV() and ppc64le checks
    Since creating tools and lib files, this commit attempts to address
    the multi-arch optimizations for ppc64le, aka POWER hardware.
    
    HyperKit and HyperV are not supported on ppc64le so make sure
    the ARCH is not ppc64le to proceed.
    
    Speeds up bundle creation time for POWER.
    
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- createdisk.sh
+++ createdisk.sh
@@ -88,37 +88,7 @@ ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo rm -f /var/lib/kubelet/co
 # Remove audit logs
 ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo find /var/log/ -iname "*.log" -exec rm -f {} \;'
 
-if [[ ${OKD_VERSION} != "none" ]]
-then
-    # Install the hyperV rpms to VM
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo sed -i -z s/enabled=0/enabled=1/ /etc/yum.repos.d/fedora.repo'
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo sed -i -z s/enabled=0/enabled=1/ /etc/yum.repos.d/fedora-updates.repo'
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo rpm-ostree install --allow-inactive hyperv-daemons'
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo sed -i -z s/enabled=1/enabled=0/ /etc/yum.repos.d/fedora.repo'
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo sed -i -z s/enabled=1/enabled=0/ /etc/yum.repos.d/fedora-updates.repo'
-else
-    # Download the hyperV daemons dependency on host
-    mkdir $1/packages
-    sudo yum install -y --downloadonly --downloaddir $1/packages hyperv-daemons
-
-    # SCP the downloaded rpms to VM
-    ${SCP} -r $1/packages core@api.${CRC_VM_NAME}.${BASE_DOMAIN}:/home/core/
-
-    # Install the hyperV rpms to VM
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'sudo rpm-ostree install /home/core/packages/*.rpm'
-
-    # Remove the packages from VM
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- rm -fr /home/core/packages
-
-    # Cleanup up packages
-    rm -fr $1/packages
-fi
-
-# Adding Hyper-V vsock support
-
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} 'sudo bash -x -s' <<EOF
-    echo 'CONST{virt}=="microsoft", RUN{builtin}+="kmod load hv_sock"' > /etc/udev/rules.d/90-crc-vsock.rules
-EOF
+prepare_hyperV "$1"
 
 # Add gvisor-tap-vsock service
 ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} 'sudo bash -x -s' <<EOF
@@ -186,21 +156,24 @@ copy_additional_files "$1" "$libvirtDestDir"
 create_tarball "$libvirtDestDir"
 
 # HyperKit image generation
-# This must be done after the generation of libvirt image as it reuse some of
-# the content of $libvirtDestDir
-hyperkitDestDir="crc_hyperkit_${destDirSuffix}"
-mkdir "$hyperkitDestDir"
-generate_hyperkit_directory "$libvirtDestDir" "$hyperkitDestDir" "$1" "$kernel_release" "$kernel_cmd_line"
-create_tarball "$hyperkitDestDir"
+if [ "${ARCH}" != "ppc64le" ]; then
+    # This must be done after the generation of libvirt image as it reuse some of
+    # the content of $libvirtDestDir
+    hyperkitDestDir="crc_hyperkit_${destDirSuffix}"
+    mkdir "$hyperkitDestDir"
+    generate_hyperkit_directory "$libvirtDestDir" "$hyperkitDestDir" "$1" "$kernel_release" "$kernel_cmd_line"
+    create_tarball "$hyperkitDestDir"
+fi
 
 # HyperV image generation
-#
-# This must be done after the generation of libvirt image as it reuses some of
-# the content of $libvirtDestDir
-hypervDestDir="crc_hyperv_${destDirSuffix}"
-mkdir "$hypervDestDir"
-generate_hyperv_directory "$libvirtDestDir" "$hypervDestDir"
-create_tarball "$hypervDestDir"
+if [ "${ARCH}" != "ppc64le" ]; then
+    # This must be done after the generation of libvirt image as it reuses some of
+    # the content of $libvirtDestDir
+    hypervDestDir="crc_hyperv_${destDirSuffix}"
+    mkdir "$hypervDestDir"
+    generate_hyperv_directory "$libvirtDestDir" "$hypervDestDir"
+    create_tarball "$hypervDestDir"
+fi
 
 # Cleanup up vmlinux/initramfs files
 rm -fr "$1/vmlinuz*" "$1/initramfs*"
