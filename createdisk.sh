56cc21344e7b66108b77e2ed350a29e233c22e11
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2021-03-02 18:11:14 +0530

    Use systemd unit file to create dummy network
    In 4.8 nightly builds, it is observed that the changes for dummy
    network not presisted during the reboot. Looking into the networkManager
    logs doesn't provide any suspious activity. This patch will create
    a systemd unit file which will invoke nmcli recreate the
    dummy interface each time the system boots

--- createdisk.sh
+++ createdisk.sh
@@ -100,7 +100,22 @@ kernel_cmd_line=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- 'cat /proc/cm
 ${SCP} core@api.${CRC_VM_NAME}.${BASE_DOMAIN}:/boot/ostree/${BASE_OS}-${ostree_hash}/* $1
 
 # Add a dummy network interface with internalIP
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- "sudo nmcli conn add type dummy ifname eth10 con-name internalEtcd ip4 ${INTERNAL_IP}/24  && sudo nmcli conn up internalEtcd"
+${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- "cat <<EOF | sudo tee /etc/systemd/system/dummy-network.service
+[Unit]
+Description=Create dummy network
+After=NetworkManager.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/nmcli conn add type dummy ifname eth10 autoconnect yes save yes con-name internalEtcd ip4 ${INTERNAL_IP}/24
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo systemctl daemon-reload
+${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo systemctl enable dummy-network.service
 
 # Add internalIP as node IP for kubelet systemd unit file
 # More details at https://bugzilla.redhat.com/show_bug.cgi?id=1872632
