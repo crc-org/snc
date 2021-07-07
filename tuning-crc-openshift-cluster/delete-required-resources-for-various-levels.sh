25ea26fa132bca9fc8793be9020dea33d5ffe91c
SriKrishna Paparaju <paparaju@gmail.com>
2021-07-07 18:00:52 -0400

    enable  build for level-1

--- tuning-crc-openshift-cluster/delete-required-resources-for-various-levels.sh
+++ tuning-crc-openshift-cluster/delete-required-resources-for-various-levels.sh
@@ -2,7 +2,7 @@
 
 ### Create the disk image as per the level (1,2,3) indicated
 
-source ./tuning-crc-openshift-cluster/delete-required-resources-for-level-3.sh
+#source ./tuning-crc-openshift-cluster/delete-required-resources-for-level-3.sh
 source ./tuning-crc-openshift-cluster/copy-required-scripts-for-level-based-user-experience.sh
 
 
