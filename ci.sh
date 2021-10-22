be9a4ff476451acdd89e8ddce4c1f394b0381f0c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2021-10-22 10:04:42 +0530

    Remove openshift-tests steps from CI

--- ci.sh
+++ ci.sh
@@ -3,13 +3,11 @@
 set -exuo pipefail
 
 sudo yum install -y podman make golang rsync
+export PODMAN_VERSION=3.3.1 
 
 ./shellcheck.sh
 ./snc.sh
-
-mkdir -p /tmp/artifacts
-export KUBECONFIG="${HOME}"/.crc/machines/crc/kubeconfig
-openshift-tests run kubernetes/conformance --dry-run  | grep -F -v -f /tmp/ignoretests.txt  | openshift-tests run -o /tmp/artifacts/e2e.log --junit-dir /tmp/artifacts/junit -f -
+./createdisk.sh crc-tmp-dir
 rc=$?
 echo "${rc}" > /tmp/test-return
 set -e
