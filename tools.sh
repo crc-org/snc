cb46d9f9a312fb2d8e2035a8232e9d8c677e41f5
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2022-11-14 13:07:32 +0530

    Pass vm name and ip to shutdown/start functions
    This will help to reuse these function for microshift bundle creation.

--- tools.sh
+++ tools.sh
@@ -130,21 +130,22 @@ function get_vm_prefix {
 }
 
 function shutdown_vm {
-    local vm_prefix=$1
-    retry sudo virsh shutdown ${vm_prefix}-master-0
+    local vm_name=$1
+    retry sudo virsh shutdown ${vm_name}
     # Wait till instance started successfully
-    until sudo virsh domstate ${vm_prefix}-master-0 | grep shut; do
-        echo " ${vm_prefix}-master-0 still running"
+    until sudo virsh domstate ${vm_name} | grep shut; do
+        echo " ${vm_name} still running"
         sleep 3
     done
 }
 
 function start_vm {
-    local vm_prefix=$1
-    retry sudo virsh start ${vm_prefix}-master-0
+    local vm_name=$1
+    local vm_ip=$2
+    retry sudo virsh start ${vm_name}
     # Wait till ssh connection available
-    until ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- "exit 0" >/dev/null 2>&1; do
-        echo " ${vm_prefix}-master-0 still booting"
+    until ${SSH} core@${vm_ip} -- "exit 0" >/dev/null 2>&1; do
+        echo " ${vm_name} still booting"
         sleep 2
     done
 }
