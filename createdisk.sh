408d977a211a9cb4baa170a05742b1f56845fcbd
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-03-20 14:27:49 +0530

    Add wait_for_ssh helper
    This helper is used to wait till ssh connection available from the
    VM instead putting a sleep or a random wait time.

--- createdisk.sh
+++ createdisk.sh
@@ -41,6 +41,8 @@ fi
 
 VM_IP=$(sudo virsh domifaddr ${VM_NAME} | tail -2 | head -1 | awk '{print $4}' | cut -d/ -f1)
 
+wait_for_ssh ${VM_NAME} ${VM_IP}
+
 if [ ${BUNDLE_TYPE} != "microshift" ]; then
     # Remove unused images from container storage
     ${SSH} core@${VM_IP} -- 'sudo crictl rmi --prune'
