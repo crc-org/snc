d1b71f1278c7fb2e1fef8b62baa3dcd90c3a6358
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-04-28 16:53:24 +0530

    CI: Run podman bundle with crc
    This PR make sure we run the podman bundles which is created
    by CI using crc. It will make sure produced bundles are able to
    run without any issue.

--- ci.sh
+++ ci.sh
@@ -5,6 +5,18 @@ set -exuo pipefail
 ./shellcheck.sh
 ./snc.sh
 ./createdisk.sh crc-tmp-install-data
+
+git clone https://github.com/code-ready/crc.git
+pushd crc
+make cross
+sudo mv out/linux-amd64/crc /usr/local/bin/
+popd
+
+crc config set bundle crc_podman_libvirt_*.crcbundle
+crc config set preset podman
+crc setup
+crc start
+
 rc=$?
 echo "${rc}" > /tmp/test-return
 set -e
