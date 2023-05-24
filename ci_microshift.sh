4d837753fbed06b0d5742be6195bc4521a160853
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-05-24 18:14:54 +0530

    CI: Don't build mac bundle because kernel downgrade need internal repo access
    Mac bundle need kernel downgrade and to get those old kernel rpm, it
    need access to internal repo which is not accessible by openshift CI.

--- ci_microshift.sh
+++ ci_microshift.sh
@@ -5,12 +5,12 @@ set -exuo pipefail
 sudo yum install -y make golang
 
 ./shellcheck.sh
-./microshift.sh
+SNC_GENERATE_MACOS_BUNDLE=0  ./microshift.sh
 
 # Set the zstd compression level to 10 to have faster
 # compression while keeping a reasonable bundle size.
 export CRC_ZSTD_EXTRA_FLAGS="-10"
-./createdisk.sh crc-tmp-install-data
+SNC_GENERATE_MACOS_BUNDLE=0  ./createdisk.sh crc-tmp-install-data
 
 git clone https://github.com/crc-org/crc.git
 pushd crc
