4277eebd7db6127889f72659570529b1e78e4826
Christophe Fergeau <cfergeau@redhat.com>
2023-11-27 16:17:58 +0100

    Use latest released rhel-9.0.z kernel
    The kernel we used so far was from October 2022, which is unexpected, we
    intended to pick up the latest 9.0.z kernel.
    
    This happens because later updates where no longer released in
    rhel-9-for-x86_64-baseos-rpms but in the EUS repos. To make things more
    complicated, the EUS repo content apparently depends on the RHEL minor
    version being used.
    
    This is why this commit makes use of both --releasever=9.0 and
    --repo="rhel-9-for-x86_64-baseos-eus-rpms" to download the packages.
    
    This runs from an ubi9 container because even after forcing this, I was
    not getting the expected result on my RHEL9.2 machine.
    
    This fixes https://github.com/crc-org/snc/issues/826
    
    This also fixes macOS x86_64 microshift bundles which fail to boot.
    Without this change, the kernel modules can't be loaded, leading to boot
    failures.

--- createdisk-library.sh
+++ createdisk-library.sh
@@ -233,10 +233,9 @@ function install_additional_packages() {
 function downgrade_rhel9_kernel {
     local vm_ip=$1
     local pkgDir=$(mktemp -d tmp-rpmXXX)
-    kernel_version=$(sudo dnf --quiet repoquery-na --queryformat '%{version}-%{release}.%{arch}' kernel.${ARCH} | grep el9_0 | tail -n 1)
 
     mkdir -p ${pkgDir}/packages
-    sudo yum download --downloadonly --downloaddir ${pkgDir}/packages kernel-${kernel_version} kernel-modules-extra-${kernel_version} kernel-core-${kernel_version} kernel-modules-${kernel_version} --resolve
+    podman run --rm -v "./${pkgDir}/packages":/packages:z registry.access.redhat.com/ubi9 yum --releasever=9.0 --repo="rhel-9-for-x86_64-baseos-eus-rpms" download --downloaddir /packages kernel kernel-modules-extra kernel-core kernel-modules
     ${SCP} -r ${pkgDir}/packages core@${vm_ip}:/home/core/
     ${SSH} core@${vm_ip} -- 'SYSTEMD_OFFLINE=1 sudo -E rpm-ostree override replace --remove=kernel-modules-core /home/core/packages/*.rpm'
     ${SSH} core@${vm_ip} -- rm -fr /home/core/packages
