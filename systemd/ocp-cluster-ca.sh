6d3929169a621028e22396665fce77933ba2feb3
Anjan Nath <kaludios@gmail.com>
2024-10-09 18:41:51 +0530

    add systemd services for configuration after start
    the services does the various needed tasks to setup
    the ocp or microshift cluster, these systemd  units
    runs small shell scripts which are based on:
    https://github.com/crc-org/crc-cloud/blob/main/pkg/bundle/setup/clustersetup.sh
    
    and does the following tasks:
    - creates crc specific configurations for dnsmasq
    - sets a new uuid as cluster id
    - creates the pod for routes-controller
    - tries to grow the disk and filesystem
    - checks if the cluster operators are ready
    - adds the pull secret to the cluster
    - sets kubeadmin and developer user passwords
    - sets a custom ca for authentication
    - sets custom nip.io cluster domain

---
+++ systemd/ocp-cluster-ca.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+
+source /usr/local/bin/crc-systemd-common.sh
+export KUBECONFIG="/opt/kubeconfig"
+
+wait_for_resource configmap
+
+custom_ca_path=/opt/crc/custom-ca.crt
+
+if [ ! -f ${custom_ca_path} ]; then
+    echo "Cert bundle /opt/crc/custom-ca.crt not found"
+    exit 1
+fi
+
+if [ ! -f /opt/crc/pass_kubeadmin ]; then
+    echo "kubeadmin password file not found"
+    exit 1
+fi
+
+PASS_KUBEADMIN="$(cat /opt/crc/pass_kubeadmin)"
+
+oc create configmap client-ca-custom -n openshift-config --from-file=ca-bundle.crt=${custom_ca_path}
+oc patch apiserver cluster --type=merge -p '{"spec": {"clientCA": {"name": "client-ca-custom"}}}'
+oc create configmap admin-kubeconfig-client-ca -n openshift-config --from-file=ca-bundle.crt=${custom_ca_path} \
+--dry-run -o yaml | oc replace -f -
+
+rm -f /opt/crc/custom-ca.crt
+
+echo "Logging in again to update $KUBECONFIG"
+COUNTER=0
+MAXIMUM_LOGIN_RETRY=500
+until `oc login --insecure-skip-tls-verify=true -u kubeadmin -p "$PASS_KUBEADMIN" https://api.crc.testing:6443 > /dev/null 2>&1`
+do
+    [ $COUNTER == $MAXIMUM_LOGIN_RETRY ] && echo "Unable to login to the cluster..., installation failed." && exit 1
+    echo "Logging into OpenShift with updated credentials try $COUNTER, hang on...."
+    sleep 5
+    ((COUNTER++))
+done
