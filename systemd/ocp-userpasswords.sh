abe8a14b2493a125294a7271ef1858489dfaec2a
Anjan Nath <kaludios@gmail.com>
2024-10-09 18:41:51 +0530

    add systemd services for configuration after start
    the services does the various needed tasks to setup
    the ocp or microshift cluster, these systemd  units
    runs small shell scripts which are based on:
    https://github.com/crc-org/crc-cloud/blob/main/pkg/bundle/setup/clustersetup.sh
    
    and does the following tasks:
    - creates crc specific configurations for dnsmasq
    - sets a new uuid as cluster id
    - creates the pod for routes-controller
    - tries to grow the disk and filesystem
    - checks if the cluster operators are ready
    - adds the pull secret to the cluster
    - sets kubeadmin and developer user passwords
    - sets a custom ca for authentication
    - sets custom nip.io cluster domain

---
+++ systemd/ocp-userpasswords.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+
+source /usr/local/bin/crc-systemd-common.sh
+export KUBECONFIG="/opt/kubeconfig"
+
+function gen_htpasswd() {
+    if [ ! -z "${1}" ] && [ ! -z "${2}" ]; then
+        podman run --rm -ti xmartlabs/htpasswd $1 $2 >> /tmp/htpasswd.txt
+    fi
+}
+
+wait_for_resource secret
+
+if [ ! -f /opt/crc/pass_developer ]; then
+    echo "developer password does not exist"
+    exit 1
+fi
+
+if [ ! -f /opt/crc/pass_kubeadmin ]; then
+    echo "developer password does not exist"
+    exit 1
+fi
+
+PASS_DEVELOPER=$(cat /opt/crc/pass_developer)
+PASS_KUBEADMIN=$(cat /opt/crc/pass_kubeadmin)
+
+rm -f /tmp/htpasswd.txt
+gen_htpasswd developer "${PASS_DEVELOPER}"
+gen_htpasswd kubeadmin "${PASS_KUBEADMIN}"
+
+if [ -f /tmp/htpasswd.txt ]; then
+    sed -i '/^\s*$/d' /tmp/htpasswd.txt
+
+    oc create secret generic htpass-secret  --from-file=htpasswd=/tmp/htpasswd.txt -n openshift-config --dry-run=client -o yaml > /tmp/htpass-secret.yaml
+    oc replace -f /tmp/htpass-secret.yaml
+fi
