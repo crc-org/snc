609bd40b6169f22fb51f24636ac5ebc188106c26
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-02-06 21:37:51 +0530

    shellcheck: Use arch specific binary

--- shellcheck.sh
+++ shellcheck.sh
@@ -6,7 +6,8 @@ SHELLCHECK=${SHELLCHECK:-shellcheck}
 if ! "${SHELLCHECK}" -V; then
     if [[ ! -e SHELLCHECK ]]; then
         scversion="stable"
-        curl -L "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+        arch=$(uname -m)
+        curl -L "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.${arch}.tar.xz" | tar -xJv
     fi
     SHELLCHECK="./shellcheck-${scversion}/shellcheck"
 fi
