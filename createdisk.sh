22bcbcfc29eebad472a3159b97d8f40e5c88e1af
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-03-19 17:29:21 +0530

    Implement local repo inside the VM instance for additional packages
    During cloud-init package installation it is observered if `*.rpm` used
    which include required package and all dependencies then installed
    dependencies is not going to override and you need to first uninstall
    those or remove the that package from *.rpm list which is not ideal.
    With current approach it is easy to installed package just using the
    required package name since all deps is part of local repo and if
    package already installed it can be ignored.

--- createdisk.sh
+++ createdisk.sh
@@ -140,6 +140,8 @@ fi
 
 copy_systemd_units
 
+# Beyond this point, packages added to the ADDITIONAL_PACKAGES variable won’t be installed in the guest
+install_additional_packages ${VM_IP}
 cleanup_vm_image ${VM_NAME} ${VM_IP}
 
 # Delete all the pods except openshift-multus (which have file for crio cni config)
