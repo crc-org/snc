82f217e056354988312537e1e0300b9a033af46d
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-09-09 10:45:39 +0530

    Cloud Env: Remove autodetection of cloud environments and use CRC_CLOUD env
    Services which are using this script as check shouldn't depend on cloud
    or non cloud env because as part of next generation CRC self sufficient
    bundle should run those scripts also without depending on the cloud
    env.
    
    With this PR if user want to run those services as part of start then
    can configure `CRC_CLOUD` env as part of cloud init file.

--- systemd/crc-check-cloud-env.sh
+++ systemd/crc-check-cloud-env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-if grep -qi 'ec2' /sys/class/dmi/id/product_uuid ||
-   grep -qi 'google' /sys/class/dmi/id/product_name ||
-   grep -qi 'openstack' /sys/class/dmi/id/product_name ||
-   grep -qi '7783-7084-3265-9085-8269-3286-77' /sys/class/dmi/id/chassis_asset_tag; then
-    exit 0
-else
+if [ -z $CRC_CLOUD ]; then
     exit 1
 fi
+exit 0
