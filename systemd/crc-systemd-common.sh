ec9f6605f3b49c05c3c5cf34aed3daa707cb6349
Kevin Pouget <kpouget@redhat.com>
2025-09-19 13:24:57 +0200

    systemd: crc-systemd-common: remove the unexpected command substitution

--- systemd/crc-systemd-common.sh
+++ systemd/crc-systemd-common.sh
@@ -4,7 +4,7 @@ function wait_for_resource() {
     local retry=0
     local max_retry=${2:-20}
     local wait_sec=${3:-5}
-    until `oc get "$1" > /dev/null 2>&1`
+    until oc get "$1" > /dev/null 2>&1
     do
         [ $retry == $max_retry ] && exit 1
         sleep $wait_sec
