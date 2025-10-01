a2b9563551a24495b2502a7987e3096c05b4ea52
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:31:59 +0200

    crc-self-sufficient-env.sh: minor syntax improvements

--- systemd/crc-self-sufficient-env.sh
+++ systemd/crc-self-sufficient-env.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-set -euo pipefail
-# Optional: load env if unit forgot EnvironmentFile
-[ -r /etc/sysconfig/crc-env ] && . /etc/sysconfig/crc-env
-if [ "${CRC_SELF_SUFFICIENT:-}" = "1" ] || [ "${CRC_CLOUD:-}" = "1" ]; then
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
+
+source /etc/sysconfig/crc-env || echo "WARNING: crc-env not found"
+
+if (( ${CRC_SELF_SUFFICIENT:-0} == 1 )); then
+    echo "Running with a self sufficient bundle"
     exit 0
 fi
+
+echo "Not running in a self-sufficient or cloud environment"
+
 exit 1
