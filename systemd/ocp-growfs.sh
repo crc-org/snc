082dfd39fa01ddf34b77fdd9c86b3e107abd78ab
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:44:48 +0200

    ocp-growfs.sh: syntax improvements

--- systemd/ocp-growfs.sh
+++ systemd/ocp-growfs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
 set -x
 
 root_partition=$(/usr/sbin/blkid -t TYPE=xfs -o device)
@@ -8,4 +12,9 @@ root_partition=$(/usr/sbin/blkid -t TYPE=xfs -o device)
 rootFS="/sysroot"
 mount -o remount,rw "${rootFS}"
 xfs_growfs "${rootFS}"
+
 #mount -o remount,ro "${rootFS}"
+
+echo "All done"
+
+exit 0
