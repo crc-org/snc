85ebd267d03d26aefdae60f957159b27aaeed107
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:26:16 +0200

    crc-cluster-status: improve the bash syntax
    * Clarification of the login test/retry logic.
    * Simple time tracking for a quick glance assessment of the wait duration
    * Better logging and constant definition

--- systemd/crc-cluster-status.sh
+++ systemd/crc-cluster-status.sh
@@ -7,6 +7,8 @@ set -o errtrace
 set -x
 
 export KUBECONFIG=/opt/kubeconfig
+MAXIMUM_LOGIN_RETRY=10
+RETRY_DELAY=5
 
 if [ ! -f /opt/crc/pass_kubeadmin ]; then
     echo "kubeadmin password file not found"
@@ -15,25 +17,49 @@ fi
 
 rm -rf /tmp/.crc-cluster-ready
 
+SECONDS=0
 if ! oc adm wait-for-stable-cluster --minimum-stable-period=1m --timeout=10m; then
     exit 1
 fi
 
+echo "Cluster took $SECONDS seconds to stabilize."
 
-echo "Logging into OpenShift with kubeadmin user to update $KUBECONFIG"
-COUNTER=1
-MAXIMUM_LOGIN_RETRY=10
+echo "Logging into OpenShift with kubeadmin user to update the KUBECONFIG"
 
-# use a `(set +x)` subshell to avoid leaking the password
-until (set +x ; oc login --insecure-skip-tls-verify=true -u kubeadmin -p "$(cat /opt/crc/pass_kubeadmin)" https://api.crc.testing:6443 > /dev/null 2>&1); do
-    if [ "$COUNTER" -ge "$MAXIMUM_LOGIN_RETRY" ]; then
-        echo "Unable to login to the cluster..., authentication failed."
+try_login() {
+    (   # use a `(set +x)` subshell to avoid leaking the password
+        set +x
+        set +e # don't abort on error in this subshell
+        oc login --insecure-skip-tls-verify=true \
+           -u kubeadmin \
+           -p "$(cat /opt/crc/pass_kubeadmin)" \
+           https://api.crc.testing:6443 > /dev/null 2>&1
+    )
+    local success="$?"
+    if [[ "$success" == 0 ]]; then
+        echo "Login succeeded"
+    else
+        echo "Login did not complete ..."
+    fi
+
+    return "$success"
+}
+
+for ((counter=1; counter<=MAXIMUM_LOGIN_RETRY; counter++)); do
+    echo "Login attempt $counter/$MAXIMUM_LOGIN_RETRY…"
+    if try_login; then
+        break
+    fi
+    if (( counter == MAXIMUM_LOGIN_RETRY )); then
+        echo "Unable to login to the cluster after $counter attempts; authentication failed."
         exit 1
     fi
-    echo "Logging into OpenShift with updated credentials try $COUNTER, hang on...."
-    sleep 5
-    ((COUNTER++))
+    sleep "$RETRY_DELAY"
 done
 
 # need to set a marker to let `crc` know the cluster is ready
 touch /tmp/.crc-cluster-ready
+
+echo "All done after $SECONDS seconds "
+
+exit 0
