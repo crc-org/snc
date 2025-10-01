0ce80162bee15508292e9b6831e02c12b0167ecb
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:48:47 +0200

    ocp-userpasswords.sh: syntax improvements
    * Better use of script constants
    * Better validation of the arguments
    * Better logging
    
    File will be further updated to prevent leaking passwords in the
    journal logs.

--- systemd/ocp-userpasswords.sh
+++ systemd/ocp-userpasswords.sh
@@ -9,41 +9,48 @@ set -x
 source /usr/local/bin/crc-systemd-common.sh
 export KUBECONFIG="/opt/kubeconfig"
 
+CRC_PASS_DEVELOPER_PATH=/opt/crc/pass_developer
+CRC_PASS_KUBEADMIN_PATH=/opt/crc/pass_kubeadmin
+CRC_HTPASSWD_IMAGE=registry.access.redhat.com/ubi10/httpd-24
+
 function gen_htpasswd() {
     if [ -z "${1:-}" ] || [ -z "${2:-}" ]; then
-        echo "gen_htpasswd needs two arguments: username password" 1>&2
+        echo "gen_htpasswd needs two arguments: username password" >&2
         return 1
     fi
 
-    podman run --rm docker.io/xmartlabs/htpasswd "$1" "$2"
+    podman run --rm "$CRC_HTPASSWD_IMAGE" htpasswd -nb "$1" "$2"
 }
 
-wait_for_resource secret
-
-if [ ! -f /opt/crc/pass_developer ]; then
-    echo "developer password does not exist"
+if [[ ! -r "$CRC_PASS_DEVELOPER_PATH" ]]; then
+    echo "ERROR: CRC developer password does not exist ($CRC_PASS_DEVELOPER_PATH)"
     exit 1
 fi
 
-if [ ! -f /opt/crc/pass_kubeadmin ]; then
-    echo "kubeadmin password does not exist"
+if [[ ! -r "$CRC_PASS_KUBEADMIN_PATH" ]]; then
+    echo "ERROR: CRC kubeadmin password does not exist ($CRC_PASS_KUBEADMIN_PATH)"
     exit 1
 fi
 
-echo "generating the kubeadmin and developer passwords ..."
+echo "Pulling $CRC_HTPASSWD_IMAGE ..."
+podman pull --quiet "$CRC_HTPASSWD_IMAGE"
 
-set +x # /!\ disable the logging to avoid leaking the passwords
+wait_for_resource_or_die secret
 
-dev_pass=$(gen_htpasswd developer "$(cat /opt/crc/pass_developer)")
-adm_pass=$(gen_htpasswd kubeadmin "$(cat /opt/crc/pass_kubeadmin)")
+echo "Generating the kubeadmin and developer passwords ..."
+set +x # disable the logging to avoid leaking the passwords
+
+dev_pass=$(gen_htpasswd developer "$(cat "$CRC_PASS_DEVELOPER_PATH")")
+adm_pass=$(gen_htpasswd kubeadmin "$(cat "$CRC_PASS_KUBEADMIN_PATH")")
 
 echo "creating the password secret ..."
-# use bash <() to use a temporary fd file
-# use sed to remove the empty lines
+# use bash "<()" to use a temporary fd file (safer to handle secrets)
 oc create secret generic htpass-secret  \
    --from-file=htpasswd=<(printf '%s\n%s\n' "$dev_pass" "$adm_pass") \
    -n openshift-config \
    --dry-run=client -oyaml \
     | oc apply -f-
 
-echo "all done"
+echo "All done"
+
+exit 0
