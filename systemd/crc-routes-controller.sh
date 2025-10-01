1ed60a340ed9c08d7d0adfcc3401c0422461c8c7
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:31:21 +0200

    crc-routes-controller.sh: minor syntax improvements
    * Make the script more resilient by failing on any error.
    * Better use of script constants
    * Switch to `wait_for_resource_or_die`

--- systemd/crc-routes-controller.sh
+++ systemd/crc-routes-controller.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
 set -x
 
+ROUTE_CONTROLLER=/opt/crc/routes-controller.yaml
 
 source /usr/local/bin/crc-systemd-common.sh
+
 export KUBECONFIG=/opt/kubeconfig
 
-wait_for_resource pods
+wait_for_resource_or_die pods
+wait_for_resource_or_die deployments
 
-oc apply -f /opt/crc/routes-controller.yaml
+oc apply -f "$ROUTE_CONTROLLER"
 
+echo "All done."
+
+exit 0
