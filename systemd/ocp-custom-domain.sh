9be6a003969060174f9a7a1aef100335bcb79cd3
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:44:32 +0200

    ocp-custom-domain.sh: syntax and reliability improvements
    * Make more resilient by failing on any error
    * Stronger verifications on the external-ip file
    * Make more readable by splitting long lines
    * Better isolation and cleanup of the temporary cert files
    * Use of `jq` to set the JSON arguments

--- systemd/ocp-custom-domain.sh
+++ systemd/ocp-custom-domain.sh
@@ -1,49 +1,116 @@
 #!/bin/bash
 
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
 set -x
 
 source /usr/local/bin/crc-systemd-common.sh
 export KUBECONFIG="/opt/kubeconfig"
 
-if [ ! -f /opt/crc/eip ]; then
-    echo "external ip not found"
+CRC_EXTERNAL_IP_FILE_PATH=/opt/crc/eip
+
+if [[ ! -r "$CRC_EXTERNAL_IP_FILE_PATH" ]]; then
+    echo "ERROR: CRC external ip file not found ($CRC_EXTERNAL_IP_FILE_PATH)"  >&2
     exit 1
 fi
 
-EIP=$(cat /opt/crc/eip)
+EIP=$(tr -d '\r\n' < "$CRC_EXTERNAL_IP_FILE_PATH")
 
-STEPS_SLEEP_TIME=30
+if [[ -z "$EIP" ]]; then
+    echo "ERROR: External IP file is empty: $CRC_EXTERNAL_IP_FILE_PATH" >&2
+    exit 1
+fi
 
-wait_for_resource secret
+# Basic IPv4 sanity check; adjust if IPv6 is expected
+if [[ ! "$EIP" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    echo "ERROR: Invalid IPv4 address read from $CRC_EXTERNAL_IP_FILE_PATH: '$EIP'" >&2
+    exit 1
+fi
+
+wait_for_resource_or_die secret
+
+TMP_KEY_FILE=$(mktemp /tmp/nip.key.XXXXX)
+TMP_CRT_FILE=$(mktemp /tmp/nip.crt.XXXXX)
+
+cleanup() {
+    rm -f "$TMP_KEY_FILE" "$TMP_CRT_FILE"
+    echo "Temp files cleanup complete."
+}
+
+# Cleanup happens automatically via trap on error or at script end
+trap cleanup ERR EXIT
 
 # create cert and add as secret
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout /tmp/nip.key -out /tmp/nip.crt -subj "/CN=$EIP.nip.io" -addext "subjectAltName=DNS:apps.$EIP.nip.io,DNS:*.apps.$EIP.nip.io,DNS:api.$EIP.nip.io"
-oc delete secret nip-secret -n openshift-config || true
-oc create secret tls nip-secret --cert=/tmp/nip.crt --key=/tmp/nip.key -n openshift-config
-sleep $STEPS_SLEEP_TIME
+openssl req -newkey rsa:2048 -new \
+        -nodes -x509 -days 3650 \
+        -keyout "$TMP_KEY_FILE" -out "$TMP_CRT_FILE" \
+        -subj "/CN=$EIP.nip.io" \
+        -addext "subjectAltName=DNS:apps.$EIP.nip.io,DNS:*.apps.$EIP.nip.io,DNS:api.$EIP.nip.io"
+
+oc delete secret nip-secret -n openshift-config --ignore-not-found
+oc create secret tls nip-secret \
+   --cert="$TMP_CRT_FILE" \
+   --key="$TMP_KEY_FILE" \
+   -n openshift-config
 
 # patch ingress
-    cat <<EOF > /tmp/ingress-patch.yaml
-spec:
-  appsDomain: apps.$EIP.nip.io
-  componentRoutes:
-  - hostname: console-openshift-console.apps.$EIP.nip.io
-    name: console
-    namespace: openshift-console
-    servingCertKeyPairSecret:
-      name: nip-secret
-  - hostname: oauth-openshift.apps.$EIP.nip.io
-    name: oauth-openshift
-    namespace: openshift-authentication
-    servingCertKeyPairSecret:
-      name: nip-secret
-EOF
-oc patch ingresses.config.openshift.io cluster --type=merge --patch-file=/tmp/ingress-patch.yaml
+wait_for_resource_or_die ingresses.config.openshift.io
+jq -n --arg eip "$EIP" '
+{
+  "spec": {
+    "appsDomain": "apps.\($eip).nip.io",
+    "componentRoutes": [
+      {
+        "hostname": "console-openshift-console.apps.\($eip).nip.io",
+        "name": "console",
+        "namespace": "openshift-console",
+        "servingCertKeyPairSecret": {
+          "name": "nip-secret"
+        }
+      },
+      {
+        "hostname": "oauth-openshift.apps.\($eip).nip.io",
+        "name": "oauth-openshift",
+        "namespace": "openshift-authentication",
+        "servingCertKeyPairSecret": {
+          "name": "nip-secret"
+        }
+      }
+    ]
+  }
+}' | oc patch ingresses.config.openshift.io cluster --type=merge --patch-file=/dev/stdin
 
 # patch API server to use new CA secret
-oc patch apiserver cluster --type=merge -p '{"spec":{"servingCerts": {"namedCertificates":[{"names":["api.'$EIP'.nip.io"],"servingCertificate": {"name": "nip-secret"}}]}}}'
+wait_for_resource_or_die apiserver.config.openshift.io
+jq -n --arg eip "$EIP" '
+{
+  "spec": {
+    "servingCerts": {
+      "namedCertificates": [
+        {
+          "names": [
+            "api.\($eip).nip.io"
+          ],
+          "servingCertificate": {
+            "name": "nip-secret"
+          }
+        }
+      ]
+    }
+  }
+}' | oc patch apiserver cluster --type=merge --patch-file=/dev/stdin
 
 # patch image registry route
-oc patch -p '{"spec": {"host": "default-route-openshift-image-registry.'$EIP'.nip.io"}}' route default-route -n openshift-image-registry --type=merge
+wait_for_resource_or_die route.route.openshift.io
+jq -n --arg eip "$EIP" '
+{
+  "spec": {
+    "host": "default-route-openshift-image-registry.\($eip).nip.io"
+  }
+}' | oc patch route default-route -n openshift-image-registry --type=merge --patch-file=/dev/stdin
 
-#wait_cluster_become_healthy "authentication|console|etcd|ingress|openshift-apiserver"
+echo "All done"
+
+exit 0
