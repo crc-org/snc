d7076e5e30b80b680a500d1cbc546593fc1e5fcd
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:27:02 +0200

    crc-systemd-common.sh: improve the bash syntax, rename into wait_for_resource_or_die
    * Rename the function into `wait_for_resource_or_die` to make it clear
    that the function exits if the wait fails
    * Disable `set -x` during the wait, to reduce the journal verbosity
    * Check that the `$resource` argument isn't missing
    * Use of Bash arithmetic syntax to make the code more readable
    * Explicit of `for (())` and `(( retry == max_retry ))` checks to
    easily read the execution flow
    * Clear error messages

--- systemd/crc-systemd-common.sh
+++ systemd/crc-systemd-common.sh
@@ -1,15 +1,49 @@
 # $1 is the resource to check
 # $2 is an optional maximum retry count; default 20
-function wait_for_resource() {
-    local retry=0
+function wait_for_resource_or_die() {
+    local resource=${1:-}
     local max_retry=${2:-20}
     local wait_sec=${3:-5}
-    until oc get "$1" > /dev/null 2>&1
-    do
-        [[ "$retry" -ge "$max_retry" ]] && exit 1
-        sleep $wait_sec
-        ((retry++))
+
+    local xtrace_was_disabled=0
+    # Check if xtrace is currently DISABLED. If so, set a flag.
+    [[ $- == *x* ]] || xtrace_was_disabled=1
+    set +x # disable xtrace to reduce the verbosity of this function
+
+    if [[ -z "$resource" ]]; then
+        echo "ERROR: expected a K8s resource as first parameter ..."
+        echo "ERROR: wait_for_resource_or_die RESOURCE [max_retry=20] [wait_sec=5]"
+        exit 1 # this is wait_for_resource_or_die, so die ...
+    fi
+
+    # Loop from 1 up to max_retry
+    for (( retry=1; retry<=max_retry; retry++ )); do
+        # Try the command. If it succeeds, exit the loop.
+        if oc get $resource > /dev/null 2>&1; then
+            local end_time
+            end_time=$(date +%s)
+
+            local duration=$((end_time - start_time))
+            echo "Resource '$resource' found after $retry tries ($duration seconds)."
+
+            if (( ! xtrace_was_disabled )); then
+                set -x # reenable xtrace
+            fi
+
+            return 0
+        fi
+
+        # If it's the last attempt, log a failure message before exiting
+        if (( retry == max_retry )); then
+            echo "Error: Timed out waiting for resource '$resource' after ${max_retry} attempts x ${wait_sec} seconds." >&2
+            exit 1 # this is wait_for_resource_or_die, so die ...
+        fi
+
+        # Wait before the next attempt
+        echo "Attempt ${retry}/${max_retry} didn't succeed."
+        echo "Waiting $wait_sec seconds for '$resource'."
+        sleep "$wait_sec"
     done
 
-    return 0
+    # unreachable
 }
