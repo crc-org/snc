7778c29df3b0a755ebe7c15f5c009423c90d7a38
Kevin Pouget <kpouget@redhat.com>
2025-10-01 16:47:20 +0200

    ocp-mco-sshkey.sh: syntax improvements

--- systemd/ocp-mco-sshkey.sh
+++ systemd/ocp-mco-sshkey.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
 set -x
 
 source /usr/local/bin/crc-systemd-common.sh
 export KUBECONFIG="/opt/kubeconfig"
 
-pub_key_path="/opt/crc/id_rsa.pub"
+CRC_PUB_KEY_PATH="/opt/crc/id_rsa.pub"
 
-if [ ! -f "${pub_key_path}" ]; then
-    echo "No pubkey file found"
+if [[ ! -r "$CRC_PUB_KEY_PATH" ]]; then
+    echo "ERROR: CRC pubkey file does not exist ($CRC_PUB_KEY_PATH)"
     exit 1
 fi
+
+wait_for_resource_or_die machineconfig/99-master-ssh
 
 echo "Updating the public key resource for machine config operator"
-pub_key=$(tr -d '\n\r' < ${pub_key_path})
-wait_for_resource machineconfig
-if ! oc patch machineconfig 99-master-ssh -p "{\"spec\": {\"config\": {\"passwd\": {\"users\": [{\"name\": \"core\", \"sshAuthorizedKeys\": [\"${pub_key}\"]}]}}}}" --type merge;
-then
-    echo "failed to update public key to machine config operator"
-    exit 1
-fi
+pub_key=$(cat "$CRC_PUB_KEY_PATH" | tr -d '\n\r')
+
+jq -n --arg key "${pub_key}" '
+{
+  "spec": {
+    "config": {
+      "passwd": {
+        "users": [
+          {
+            "name": "core",
+            "sshAuthorizedKeys": [ $key ]
+          }
+        ]
+      }
+    }
+  }
+}' | oc patch machineconfig 99-master-ssh --type merge --patch-file=/dev/stdin
+
+echo "All done"
+
+exit 0
