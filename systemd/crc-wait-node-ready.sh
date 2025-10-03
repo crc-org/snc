d200c004629261ce3927019c64a0a723e4f6a912
Kevin Pouget <kpouget@redhat.com>
2025-10-03 10:25:26 +0200

    systemd: add synchronization on crc-wait-node-ready
    This script add a synchronization point on the `ready` status of the
    CRC node.
    
    Before the node is ready, services can interact with the K8s
    APIServer, but user (and OCP) services won't start their deployment
    before the CRC node is ready.
    
    This synchronization point avoid that other services (like the
    `ocp-wait-apiservices`) wait in vain while their target didn't start
    their own deployment.

---
+++ systemd/crc-wait-node-ready.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
+
+source /usr/local/bin/crc-systemd-common.sh
+
+MAX_RETRY=150
+WAIT_SEC=2
+NODE_NAME=node/crc
+# Loop from 1 up to max_retry
+for retry in $(seq 1 "$MAX_RETRY"); do
+    node_status=$(oc get "$NODE_NAME" --no-headers | awk '{print $2}' || true)
+    node_status=${node_status:-"<status unavailable>"}
+
+    # Check if the node status is "Ready"
+    if [[ $node_status == "Ready" ]]; then
+        echo "CRC node is ready."
+        exit 0
+    fi
+
+    echo "CRC node is not ready. Status: $node_status"
+
+    # If it's the last attempt, log a failure message before exiting
+    if (( retry == MAX_RETRY )); then
+        echo "Error: Timed out waiting for the CRC node to be ready after $MAX_RETRY attempts x $WAIT_SEC seconds." >&2
+        exit 1
+    fi
+
+    # Wait before the next attempt
+    echo "Waiting for crc node to be ready ... (Attempt ${retry}/${MAX_RETRY})"
+    sleep "$WAIT_SEC"
+done
+
+# cannot be reached
+
+exit 1
