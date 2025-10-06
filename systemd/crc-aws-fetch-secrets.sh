9d77c51b97c287970090ceb0567e7883b4be766c
Kevin Pouget <kpouget@redhat.com>
2025-10-06 16:20:48 +0200

    crc-aws-fetch-secrets.sh: new script for mapt to fetch the secrets from AWS-CLI
    This script offloads `mapt` and other AWS deployers from the task of
    fetching the secrets from AWS IMDS service.
    
    This script should be include in the `cloud-init` user-data
    configuration file, with this kind of invocation:
    ```
    /usr/local/bin/crc-aws-fetch-secrets.sh \
      "{{ .SSMPullSecretName }}" \
      "{{ .SSMKubeAdminPasswordName }}" \
      "{{ .SSMDeveloperPasswordName }}"
    ```
    
    where the parameters specify the location of the three secrets in the
    IMDS store.

---
+++ systemd/crc-aws-fetch-secrets.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
+set -x
+
+# set -x is safe, the secrets are passed via stdin
+
+AWS_CLI_IMG=docker.io/amazon/aws-cli
+MIN_CHAR_COUNT=8 # minimum number of chars for the secret to be
+                 # assumed valid
+
+umask 0077 # 0600 file permission for secrets
+install -d -m 0700 /opt/crc # ensure that the target directory exists
+
+PULL_SECRETS_KEY=${1:-}
+KUBEADM_PASS_KEY=${2:-}
+DEVELOPER_PASS_KEY=${3:-}
+
+if [[ -z "$PULL_SECRETS_KEY" || -z "$KUBEADM_PASS_KEY" || -z "$DEVELOPER_PASS_KEY" ]]; then
+    echo "ERROR: expected to receive 3 parameters: PULL_SECRETS_KEY KUBEADM_PASS_KEY DEVELOPER_PASS_KEY"
+    exit 1
+fi
+
+SECONDS=0
+podman pull --quiet "$AWS_CLI_IMG"
+echo "Took $SECONDS seconds to pull the $AWS_CLI_IMG"
+
+wait_imds_available_and_get_region() {
+    total_timeout_minutes=5
+    retry_interval_seconds=5
+
+    IMDS_TOKEN_COMMAND=(
+        curl
+        --connect-timeout 1
+        -X PUT
+        "http://169.254.169.254/latest/api/token"
+        -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"
+        -Ssf
+    )
+    success=false
+    deadline=$(( $(date +%s) + (total_timeout_minutes * 60) ))
+    while [[ $(date +%s) -lt $deadline ]]; do
+        # By placing the command in an 'if' condition, we can test its exit code
+        # without triggering 'set -e'. The output is still captured.
+        if TOKEN=$("${IMDS_TOKEN_COMMAND[@]}"); then
+            # This block only runs if the curl command succeeds (exit code 0)
+            success=true
+            echo "Successfully fetched token." >&2
+            break # Exit the loop on success
+        fi
+
+        # This block runs if the curl command fails
+        echo "Failed to connect. Retrying in $retry_interval_seconds seconds..." >&2
+        sleep "$retry_interval_seconds"
+    done
+
+    if [[ "$success" != "true" ]]; then
+        echo "ERROR: Could not fetch token after $total_timeout_minutes minutes." >&2
+        return 1
+    fi
+
+    # Then, use the token to get the region
+    echo "Fetching the AWS region ..."
+    curl -Ssf -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region > /tmp/aws-region
+    echo >> /tmp/aws-region # add EOL at EOF, for consistency
+    echo "AWS region: $(< /tmp/aws-region)"
+}
+
+(
+    set +x # disable the xtrace as the token would be leaked
+    echo "Waiting for the AWS IMDS service to be available ..."
+    SECONDS=0
+    wait_imds_available_and_get_region
+    echo "Took $SECONDS for the IMDS service to become available."
+)
+
+missing_secrets=0
+
+save_secret() {
+    name=$1
+    key=$2
+    dest=$3
+
+    # --log-driver=none avoids that the journal captures the stdout
+    # logs of podman and leaks the passwords in the journal ...
+    if ! podman run \
+           --name "cloud-init-fetch-$name" \
+           --env AWS_REGION="$(< /tmp/aws-region)" \
+           --rm \
+           "$AWS_CLI_IMG" \
+           ssm get-parameter \
+               --name "$key" \
+               --with-decryption \
+               --query "Parameter.Value" \
+               --output text \
+            > "${dest}.tmp"
+    then
+        rm -f "${dest}.tmp"
+        echo "ERROR: failed to get the '$name' secret ... (fetched from $key)"
+        ((missing_secrets += 1))
+        return
+    fi
+    char_count=$(wc -c < "${dest}.tmp")
+    if (( char_count < MIN_CHAR_COUNT )); then
+        echo "ERROR: the content of the '$name' secret is too short ... (fetched from $key)"
+        rm -f "${dest}.tmp"
+        ((missing_secrets += 1))
+        return
+    fi
+
+    mv "${dest}.tmp" "${dest}" # atomic creation of the file
+}
+
+save_secret "pull-secrets" "$PULL_SECRETS_KEY" /opt/crc/pull-secret
+save_secret "kubeadmin-pass" "$KUBEADM_PASS_KEY" /opt/crc/pass_kubeadmin
+save_secret "developer-pass" "$DEVELOPER_PASS_KEY" /opt/crc/pass_developer
+
+if (( missing_secrets != 0 )); then
+    echo "ERROR: failed to fetch $missing_secrets secrets ..."
+    exit 1
+fi
+
+exit 0
