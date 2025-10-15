13773b4609343dc7b513a3cc1ea4482c1a6bc594
Kevin Pouget <kpouget@redhat.com>
2025-10-15 09:59:56 +0200

    systemd: update the self-sufficient/user-mode tests to avoid relying on the env file

--- systemd/crc-self-sufficient-env.sh
+++ systemd/crc-self-sufficient-env.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# set -o errexit disabled to capture the test return code
 set -o pipefail
-set -o errexit
 set -o nounset
 set -o errtrace
 
-source /etc/sysconfig/crc-env || echo "WARNING: crc-env not found"
+TEST_TIMEOUT=120
+VSOCK_COMM_PORT=1024
 
-if (( ${CRC_SELF_SUFFICIENT:-0} == 1 )); then
-    echo "Running with a self-sufficient bundle"
-    exit 0
-else
-    echo "Not running in a self-sufficient bundle"
-    exit 1
-fi
+timeout "$TEST_TIMEOUT" python3 /usr/local/bin/crc-test-vsock.py "$VSOCK_COMM_PORT"
+returncode=$?
+
+case "$returncode" in
+    124)
+        echo "ERROR: vsock/${VSOCK_COMM_PORT} test timed out after $TEST_TIMEOUT seconds :/" >&2
+        exit 124
+        ;;
+    1)
+        echo "vsock/${VSOCK_COMM_PORT} not working, running with a self-sufficient bundle" >&2
+        exit 0
+        ;;
+    0)
+        echo "vsock/${VSOCK_COMM_PORT} works, not running with a self-sufficient bundle" >&2
+        exit 1
+        ;;
+    *)
+        echo "ERROR: unexpected return code from the vsock test: $returncode" >&2
+        exit "$returncode"
+esac
+
+# cannot be reached
---
+++ systemd/crc-test-vsock.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+import time
+import fcntl, struct
+import os
+import errno
+import pathlib
+
+VSOCK_DEV = pathlib.Path("/dev/vsock")
+HOST_CID = 2 # VMADDR_CID_HOST
+
+def main():
+    if len(sys.argv) != 2:
+        print("ERROR: expected a vsock port number as first argument.")
+        raise SystemExit(errno.EINVAL)
+
+    port = int(sys.argv[1])
+    tries = 60
+    while not VSOCK_DEV.exists():
+        tries -= 1
+
+        if not tries:
+            print(f"ERROR: {VSOCK_DEV} didn't appear ...")
+            return errno.ENODEV
+        print(f"Waiting for {VSOCK_DEV} to appear ... ({tries} tries left)")
+        time.sleep(1)
+
+    print(f"Looking up the CID in {VSOCK_DEV}...")
+    with open(VSOCK_DEV, 'rb') as f:
+        r = fcntl.ioctl(f, socket.IOCTL_VM_SOCKETS_GET_LOCAL_CID, '    ')
+        cid = struct.unpack('I', r)[0]
+    print(f'Our vsock CID is {cid}.')
+
+    s = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+
+    try:
+        s.connect((HOST_CID, port))
+    except OSError as e:
+
+        if e.errno in (errno.ENODEV, errno.ECONNREFUSED, errno.EHOSTUNREACH, errno.ETIMEDOUT, errno.ECONNRESET):
+            print(f"No remote host on vsock://{HOST_CID}:{port} ({e.strerror})")
+            s.close()
+            return 1
+
+        print(f"Unexpected error connecting vsock://{HOST_CID}:{port}: {e}")
+        s.close()
+        return 1
+
+    msg = b"hello"
+    s.sendall(msg)
+
+    s.sendall(b"\n")
+
+    s.close()
+    print(f"A remote host is listening on vsock://{HOST_CID}:{port}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
