41fc03b903680b0cc338ddb7905fdc9cbec05c62
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2026-05-18 21:22:32 +0530

    CI: Use oc binary for respective bundle
    Since we are now using public image which doesn't have oc binary
    installed so copy the downloaded oc binary for respective bundle and
    put that in /usr/local/bin directory.

--- ci.sh
+++ ci.sh
@@ -9,6 +9,7 @@ sudo yum install -y git-core virtiofsd podman make golang rsync
 
 echo "### Extracting openshift-tests binary"
 mkdir /tmp/os-test
+sudo cp ./openshift-clients/linux/oc /usr/local/bin/
 export TESTS_IMAGE=$(oc --kubeconfig=crc-tmp-install-data/auth/kubeconfig adm release info -a "${HOME}"/pull-secret --image-for=tests)
 oc image extract -a "${HOME}"/pull-secret "${TESTS_IMAGE}" --path=/usr/bin/openshift-tests:/tmp/os-test/.
 chmod +x /tmp/os-test/openshift-tests
