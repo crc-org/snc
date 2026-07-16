426e410784a30172110bbef268bbbfda1c716b58
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2026-07-16 16:35:25 +0530

    Remove unused BASE_OS variable, use BUNDLE_TYPE instead
    The BASE_OS variable was introduced in 3473878 to differentiate the
    base operating system (rhcos, fedora-coreos, rhel) across bundle types.
    It was primarily used for extracting ostree hash, kernel/initramfs paths,
    and LUKS handling which are all OS-specific.
    
    Since 6b0e584 removed the initramfs/vmlinuz and ostree hash extraction
    code, BASE_OS is no longer needed. The only remaining usage was in
    install_additional_packages() to check for fedora-coreos, which maps
    directly to the okd bundle type. Replace that check with BUNDLE_TYPE
    and remove BASE_OS entirely.

--- createdisk-library.sh
+++ createdisk-library.sh
@@ -167,7 +167,7 @@ function copy_additional_files {
 function install_additional_packages() {
     local vm_ip=$1
     shift
-    if [[ ${BASE_OS} = "fedora-coreos" ]]; then
+    if [[ ${BUNDLE_TYPE} = "okd" ]]; then
         ${SSH} core@${vm_ip} -- 'sudo sed -i -z s/enabled=0/enabled=1/g /etc/yum.repos.d/centos.repo'
         ${SSH} core@${vm_ip} -- "sudo rpm-ostree install --allow-inactive $ADDITIONAL_PACKAGES"
         ${SSH} core@${vm_ip} -- 'sudo sed -i -z s/enabled=1/enabled=0/g /etc/yum.repos.d/centos.repo'
